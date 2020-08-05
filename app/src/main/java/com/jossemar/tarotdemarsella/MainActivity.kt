308cbc780f3961c1c062e7203190d038d0d8b5f5
Jossemar Esquivel <linder_reynae@hotmail.com>
2020-08-04 23:53:54 -0500

    Add side menu and main activity

---
+++ app/src/main/java/com/jossemar/tarotdemarsella/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.jossemar.tarotdemarsella
+
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.jossemar.tarotdemarsella.ui.home.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentsContainer, HomeFragment()).commit()
+        }
+
+    }
+
+}
